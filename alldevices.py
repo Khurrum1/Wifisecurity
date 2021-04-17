b5e2e4433efbe2796bd00fb3f0e3f50cd1631f0b
Khurrum1 <35710074+Khurrum1@users.noreply.github.com>
2021-04-17 15:51:06 +0100

    Add files via upload

---
+++ alldevices.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+import sys
+from scapy.all import *
+
+devices = set() #defining a set that holds all MAC addresses of all discoverable WIFI devices
+
+def PacketHandler(pkt): #defining the packet handler that receives the packet as input and ensuring it is a 802.11 packet
+	if pkt.haslayer(Dot11) :
+
+		dot11_layer = pkt.getlayer(Dot11)
+		if dot11_layer.addr2 and ( dot11_layer.addr2 not in devices ) :
+			devices.add(dot11_layer.addr2) 
+			print len(devices), dot_11.layer.addr2, dot11_layer.payload.name # prints the number of devices,  MAC addresses, data frame
+
+sniff(iface = sys.argv[1], count =  int( sys.argv[2] ), prn = PacketHandler)
+

c6e94e013087ea39e3d165ae09416b6ed86e6741
Khurrum1 <35710074+Khurrum1@users.noreply.github.com>
2021-04-17 15:51:37 +0100

    Update alldevices.py

--- alldevices.py
+++ alldevices.py
@@ -11,7 +11,7 @@ def PacketHandler(pkt): #defining the packet handler that receives the packet as
 		dot11_layer = pkt.getlayer(Dot11)
 		if dot11_layer.addr2 and ( dot11_layer.addr2 not in devices ) :
 			devices.add(dot11_layer.addr2) 
-			print len(devices), dot_11.layer.addr2, dot11_layer.payload.name # prints the number of devices,  MAC addresses, data frame
+			print len(devices), dot_11.layer.addr2, dot11_layer.payload.name # prints the number of devices,  MAC addresses, and data frame
 
 sniff(iface = sys.argv[1], count =  int( sys.argv[2] ), prn = PacketHandler)
 
